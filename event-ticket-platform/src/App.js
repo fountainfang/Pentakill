69d3fac70d667bccfe7617d4d50591ee9458ae1c
andrewyang0620 <yangjingtaoitb@gmail.com>
2024-03-05 14:08:14 -0800

    change add for APP.js

--- event-ticket-platform/src/App.js
+++ event-ticket-platform/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TheaterFrontPage from './components/EventList';
 import TicketPurchase from './components/TicketPurchase';
+import EventDetails from './components/EventDetails'; // Adjust the path as necessary
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<TheaterFrontPage />} />
       <Route path="/purchase" element={<TicketPurchase />} />
+      <Route path="/event/:eventId" element={<EventDetails />} />
     </Routes>
   </BrowserRouter>
 );
