d6ba7086f99eba7fe5546d6a5e653e168a9bd57b
jcfan91 <jcfan91@hotmail.com>
2024-03-10 18:49:25 -0700

    Event Information Page Added
    -Added Event Information Page
    -Added .css style page
    -Added two pictures related to event #1

--- front-end-design/src/App.js
+++ front-end-design/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TheaterFrontPage from './Front-Page/EventList';
 import TicketPurchase from './Purchase-Page/TicketPurchase';
+import EventDetails from './EventDetail-Page/EventDetails';
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<TheaterFrontPage />} />
       <Route path="/purchase" element={<TicketPurchase />} />
+      <Route path="/event/:eventId" element={<EventDetails />} />
     </Routes>
   </BrowserRouter>
 );
