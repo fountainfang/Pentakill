db7c179e90d130ad90c4fe6316c6816a225e777b
jcfan91 <jcfan91@hotmail.com>
2024-03-12 01:47:27 -0700

    Event Creation Page and Event Info Page
    -hard coded another event info page - event id = 1
    -change previous event to event id = 0
    -added a page where we can submit forms to create a new event

--- front-end-design/src/App.js
+++ front-end-design/src/App.js
@@ -5,6 +5,7 @@ import TicketPurchase from './pages/Purchase-Page/TicketPurchase';
 import EventDetails from './pages/EventDetail-Page/EventDetails';
 import Signin from "./pages/Signin/signin"
 import Signup from "./pages/Signup/signup"
+import EventCreation from './pages/EventCreation-Page/EventCreation';
 
 const App = () => (
   <BrowserRouter>
@@ -15,6 +16,7 @@ const App = () => (
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
       <Route path="/event/:eventId" element={<EventDetails />} />
+      <Route path="/create-event" element={<EventCreation />} />
     </Routes>
   </BrowserRouter>
 );
--- front-end-design/src/pages/EventDetail-Page/EventDetails.jsx
+++ front-end-design/src/pages/EventDetail-Page/EventDetails.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import styles from './EventDetails.css';
+import Navbar from '../Front-Page/Navbar';
 
 const EventDetails = () => {
   const { eventId } = useParams();
 
   const taylorSwiftConcert = {
-    id: 1,
+    id: 0,
     title: 'TAYLOR SWIFT CONCERT',
     date: '2024-03-15',
     time: '20:00',
     location: 'Madison Square Garden, New York, NY',
     description: 'Join us for an unforgettable night with Taylor Swift, featuring all her greatest hits live in concert!',
-    bannerImage: '/images/1_banner.webp',
-    posterImage: '/images/1_profile.avif',
+    bannerImage: '/images/0_banner.webp',
+    posterImage: '/images/0_profile.avif',
   };
 
-  const event = eventId === '1' ? taylorSwiftConcert : null;
+  const phantomOfTheOperaEvent = {
+    id: 1,
+    title: 'THE PHANTOM OF THE OPERA',
+    date: '2024-04-10',
+    time: '19:00',
+    location: 'The Royal Opera House, London, UK',
+    description: 'Experience the magic of The Phantom of the Opera at the Royal Opera House. An unforgettable journey into the depths of the Paris Opera House, featuring breathtaking performances and timeless music.',
+    bannerImage: '/opera.jpg',
+    posterImage: '/s-1.jpg', 
+  };
 
+  let event;
+  if (eventId === '0') {
+    event = taylorSwiftConcert;
+  } else if (eventId === '1') {
+    event = phantomOfTheOperaEvent;
+  } else {
+    event = null;
+  }
+  
   const formattedDate = event ? new Date(event.date).toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
@@ -29,40 +48,35 @@ const EventDetails = () => {
   }
 
   return (
-    <div className="event-details">
-      <div className="menu-bar">
-        <div className="menu-links">
-          <a href="/  ">Home</a>
-          <a href="/events">Events</a>
-        </div>
-        <div className="login-link">
-          <a href="/login">Log In</a>
+<>
+  <Navbar />
+  <div className="event-details">
+    <div className="banner-image" style={{ backgroundImage: `url(${event.bannerImage})` }}></div>
+    <div className="banner-content">
+      <div className="title-and-button">
+        <h1 className="event-title">{event.title}</h1>
+      </div>
+      <p className="event-date">{formattedDate}</p>
+    </div>
+    <div className="content">
+      <div className="left-content">
+        <div className="poster">
+          <img src={event.posterImage} alt="Event Poster" className="poster-image" />
         </div>
       </div>
-      <div className="banner-image" style={{ backgroundImage: `url(${event.bannerImage})` }}></div>
-      <div className="banner-content">
-        <div className="title-and-button">
-          <h1 className="event-title">{event.title}</h1>
-
-        </div>
-        <p className="event-date">{formattedDate}</p>
-      </div>
-      <div className="content">
-        <div className="left-content"> {/* New div for left content */}
-          <div className="poster">
-            <img src={event.posterImage} alt="Event Poster" className="poster-image" />
-          </div>
-        </div>
+      <div className="right-content">
+          <button href="/buy-tickets" type="button" className="buy-ticket-button">Buy Ticket</button>
         <div className="description">
-          <a href="/buy-tickets" className="buy-ticket-btn">Buy Ticket</a>
           <p>{event.description}</p>
-          <div className="event-info">
-            <p><strong>Time:</strong> {event.time}</p>
-            <p><strong>Location:</strong> {event.location}</p>
-          </div>
+        </div>
+        <div className="event-info">
+          <p><strong>Time:</strong> {event.time}</p>
+          <p><strong>Location:</strong> {event.location}</p>
         </div>
       </div>
     </div>
+  </div>
+</>
   );
 };
 
