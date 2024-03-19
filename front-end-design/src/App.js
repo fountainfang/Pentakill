7e52bae0fd8a4256d4c4662313f304bb027bb4ac
fountainfang <119987045+fountainfang@users.noreply.github.com>
2024-03-18 19:59:44 -0700

    UPDATE

--- front-end-design/src/App.js
+++ front-end-design/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import TheaterFrontPage from './pages/Front-Page/HomePage';
 import TicketPurchase from './pages/Purchase-Page/TicketPurchase';
 import EventDetails from './pages/EventDetail-Page/EventDetails';
@@ -12,63 +12,65 @@ import Notfound from "./pages/NotFound/Notfound"
 import Userprofile from "./pages/userprofile/Userprofile"
 
 
-// const App = () => (
-//   <BrowserRouter>
-//     <Routes>
-//       <Route path="/" element={<TheaterFrontPage />} />
-//       <Route path="/purchase" element={<TicketPurchase />} />
-//       <Route path="/purchase/:eventId" element={<PurchasingPage />} />
-//       <Route path="/login" element={<Signin />} />
-//       <Route path="/signup" element={<Signup />} />
-//       <Route path="/signin" element={<Signin />} />
-//       <Route path="/event/:eventId" element={<EventDetails />} />
-//       <Route path="/create-event" element={<EventCreation />} />
-//     </Routes>
-//   </BrowserRouter>
-// );
+const App = () => (
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<TheaterFrontPage />} />
+      <Route path="/purchase" element={<TicketPurchase />} />
+      <Route path="/purchase/:eventId" element={<PurchasingPage />} />
+      <Route path="/login" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/event/:eventId" element={<EventDetails />} />
+      <Route path="/create-event" element={<EventCreation />} />
+      <Route path="/userprofile" element={<Userprofile />} />
+      <Route path="/*" element={<Notfound />} />
+    </Routes>
+  </HashRouter>
+);
 
-// export default App;
+export default App;
 
 
 
 // Simple component for displaying fetched data
-const DataDisplay = ({ data }) => (
-  <div>{data ? data : "Loading..."}</div>
-);
+// const DataDisplay = ({ data }) => (
+//   <div>{data ? data : "Loading..."}</div>
+// );
 
-const App = () => {
-  const [data, setData] = useState(null); // State to store fetched data
+// const App = () => {
+//   // const [data, setData] = useState(null); // State to store fetched data
 
-  useEffect(() => {
-    // Fetch data from the server on component mount
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []); // The empty array ensures the effect runs only once after the initial render
+//   // useEffect(() => {
+//   //   // Fetch data from the server on component mount
+//   //   fetch("/api")
+//   //     .then((res) => res.json())
+//   //     .then((data) => setData(data.message))
+//   //     .catch((error) => console.error("Error fetching data:", error));
+//   // }, []); // The empty array ensures the effect runs only once after the initial render
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<TheaterFrontPage />} />
-        <Route path="/purchase" element={<TicketPurchase />} />
-        <Route path="/purchase/:eventId" element={<PurchasingPage />} />
-        <Route path="/login" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/event/:eventId" element={<EventDetails />} />
-        <Route path="/whats-on" element={<EventTicketingPage />} />
-        {/* Dedicated route to display the fetched data */}
-        <Route path="/whats-on" element={<EventTicketingPage />} />
-        <Route path="/data" element={<DataDisplay data={data} />} />
-        <Route path="/create-event" element={<EventCreation />} />
-        <Route path="/userprofile" element={<Userprofile />} />
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<TheaterFrontPage />} />
+//         <Route path="/purchase" element={<TicketPurchase />} />
+//         <Route path="/purchase/:eventId" element={<PurchasingPage />} />
+//         <Route path="/login" element={<Signin />} />
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/signin" element={<Signin />} />
+//         <Route path="/event/:eventId" element={<EventDetails />} />
+//         <Route path="/whats-on" element={<EventTicketingPage />} />
+//         {/* Dedicated route to display the fetched data */}
+//         <Route path="/whats-on" element={<EventTicketingPage />} />
+//         {/* <Route path="/data" element={<DataDisplay data={data} />} /> */}
+//         <Route path="/create-event" element={<EventCreation />} />
+//         <Route path="/userprofile" element={<Userprofile />} />
 
-        <Route path="/*" element={<Notfound />} />
+//         <Route path="/*" element={<Notfound />} />
 
-      </Routes>
-    </BrowserRouter>
-  );
-};
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// };
 
-export default App;
+// export default App;
